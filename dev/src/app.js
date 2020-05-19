2e2dd12d63f63e84bd57cd9b57ab1b158d32e172
Hannah Grinton <hannahgrinton@gmail.com>
2020-05-19 12:10:16 -0300

    homepage, header & footer  - completely responsive

--- dev/src/app.js
+++ dev/src/app.js
@@ -1,1 +1,9 @@
-// import $ from "jquery";
+import $ from "jquery";
+
+$("#btnCollapse").click(function() {
+    $("#menuCollapse").slideToggle('medium', function() {
+        if ($(this).is(':visible'))
+            $(this).css('display','flex');
+    });
+    console.log("click");
+});
