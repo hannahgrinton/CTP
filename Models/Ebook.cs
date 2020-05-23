33b5d53474e517d43046824eb9b8568bbbc6160e
Hannah Grinton <hannahgrinton@gmail.com>
2020-05-23 13:31:12 -0300

    added ebook images

--- Models/Ebook.cs
+++ Models/Ebook.cs
@@ -14,32 +14,27 @@ namespace CTP.Models
         //code of article
         [Required]
         [MaxLength(10)]
-        [Display(Name="Code")]
-        public string code {get; set;}
+        [Display(Name="Version")]
+        public string version {get; set;}
         //author of article
         [Required]
         [MaxLength(200)]
         [Display(Name="Author")]
         public string author {get; set;}
-        //price of article
-        [Required]
-        [MaxLength(10)]
-        [Display(Name="Price")]
-        public string price {get; set;}
         //png file name
         [MaxLength(100)]
         [Display(Name="Image File")]
         public string imgFile {get; set;}
         //docx file name
-        [MaxLength(200)]
+        [MaxLength(250)]
         [Display(Name="Amazon Link")]
         public string amazon {get; set;}
         //epub file name
-        [MaxLength(100)]
+        [MaxLength(350)]
         [Display(Name="EPUB File")]
         public string epub {get; set;}
         //mobi file name
-        [MaxLength(100)]
+        [MaxLength(200)]
         [Display(Name="MOBI File")]
         public string mobi {get; set;}
     }
