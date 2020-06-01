64de9dc96bf039a01421ea37448cca0f798d7ea5
Hannah Grinton <hannahgrinton@gmail.com>
2020-06-01 11:25:38 -0300

    edit ebooks works

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -14,6 +14,7 @@ namespace CTP.Controllers {
             bookManager = myManager;
             environment = env;
         }
+
         public IActionResult Index() {
             // if (HttpContext.Session.GetString("auth") != "true") {
             //     return RedirectToAction("Index", "Login");
@@ -21,27 +22,33 @@ namespace CTP.Controllers {
             ViewBag.Current = "";
             return View(bookManager);
         }
+
         public IActionResult Ebooks() {
             ViewBag.Current = "Ebooks";
             return View(bookManager);
         }
+
         public IActionResult Ministry() {
             ViewBag.Current = "Ministry";
             return View(bookManager);
         }
+
         public IActionResult Expositions() {
             ViewBag.Current = "Booklets";
             return View(bookManager);
         }
+
         public IActionResult Booklets() {
             ViewBag.Current = "Expositions";
             return View(bookManager);
         }
+
         public IActionResult AddEbook() {
             ViewBag.Current = "";
             Ebook ebook = new Ebook();
             return View(ebook);
         }
+
         [HttpPost]
         public IActionResult AddEbookSubmit(Ebook ebook, IFormFile imgFile, IFormFile epub, IFormFile mobi) {
             if (!ModelState.IsValid) return RedirectToAction("AddEbook");
@@ -102,6 +109,7 @@ namespace CTP.Controllers {
             ebook = bookManager.getEbook(id);
             return View(ebook);
         }
+        
         [HttpPost]
         public IActionResult DeleteEbookSubmit(Ebook ebook) {
             ebook = bookManager.getEbook(ebook.id);
@@ -127,6 +135,62 @@ namespace CTP.Controllers {
             }
             return RedirectToAction("Ebooks");
         }
+        
+        [Route("/EditEbook/{id}")]
+        public IActionResult EditEbook(int id) {
+            Ebook ebook = new Ebook();
+            ebook = bookManager.getEbook(id);
+            return View(ebook);
+        }
+        
+        [HttpPost]
+        public IActionResult EditEbookSubmit(Ebook ebook, IFormFile newImgFile, IFormFile newEpub, IFormFile newMobi) {
+            if (!ModelState.IsValid) return RedirectToAction("EditEbook", ebook.id);
+            ImageManager imageManager = new ImageManager(environment, "images/covers/ebooks");
+            FileManager epubManager = new FileManager(environment, "ebooks/epub");
+            FileManager mobiManager = new FileManager(environment, "ebooks/mobi");
+            if((newImgFile != null) && (newImgFile.FileName != "") && (ebook.imgFile != newImgFile.FileName)) {
+                Console.WriteLine("\n\n\n***Image was not null so we're changing it.");
+                bool delete = imageManager.deleteImage(ebook.imgFile);
+                if (delete) {
+                    int result = imageManager.uploadImage(newImgFile);
+                    if (result == 5) {
+                        ViewData["feedback"] = "Success";
+                        Console.WriteLine("\n\n\n***Successfully uploaded image to server!***");
+                        ebook.imgFile = imageManager.fileName;
+                    } else {
+                        Console.WriteLine("\n\n\n***Error!***");
+                        return RedirectToAction("EditEbook", ebook.id);
+                    }
+                } else {
+                    Console.WriteLine("\n\n\n***There has been an error deleting old image file!***");
+                    return RedirectToAction("EditEbook", ebook.id);
+                }
+            } else {
+                Console.WriteLine("\n\n\n***Image was null so we're moving on.");
+                
+            }
+            if((newEpub != null) && (newEpub.FileName != "") && (ebook.epub != newEpub.FileName)) {
+                bool epubBool = epubManager.deleteFile(ebook.epub);
+                if (epubBool) {
+                    int epubResult = epubManager.uploadFile(newEpub);
+                    ebook.epub = epubManager.fileName;
+                }
+            } else {
+                Console.WriteLine("\n\n\n***ePub was null so we're moving on.");
+            }
+            if((newMobi != null) && (newMobi.FileName != "") && (ebook.mobi != newMobi.FileName)) {
+                bool mobiBool = mobiManager.deleteFile(ebook.mobi);
+                int mobiResult = mobiManager.uploadFile(newMobi);
+                ebook.mobi = mobiManager.fileName;
+            } else {
+                Console.WriteLine("\n\n\n***Mobi was null so we're moving on.");
+            }
+            bookManager.Update(ebook);
+            bookManager.SaveChanges();
+            return RedirectToAction("Ebooks");
+        }
+        
         public IActionResult Logout() {
             //logs user out and reqirects to login page
             HttpContext.Session.SetString("auth", "false");
